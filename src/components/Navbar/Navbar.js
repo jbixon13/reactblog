e28a6dc10d18594ec509751fa072a8c68e55e38f
jbixon13 <jbixon13@gmail.com>
2020-02-29 02:09:14 -0500

    setup structure & navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.png'
+
+require('./Navbar.scss');
+
+class Navbar extends Component {
+    render() {
+        return(
+            <div className='Navbar'>
+                <header>
+                    <a href='/'></a>
+                        <img src={logo} alt='homepage logo'></img>
+                    <div className='links'>
+                        <a href='/'>Home</a>
+                        <a href='/about'>About</a>
+                        <a href='https://www.jasonbixon.netlify.com/portfolio'>Portfolio</a>
+                        <a href='/contact'>Contact</a>
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
